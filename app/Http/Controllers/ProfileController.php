0f6eb2f2fe11b78d1311e0f9fbde785f00c49487
MahdiAghamohammadi <mahdi.develoop@gmail.com>
2024-11-07 16:18:36 +0330

    fix: Delete previous cover image on update

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -10,6 +10,7 @@ use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Storage;
 use Inertia\Inertia;
 use Inertia\Response;
 
@@ -68,15 +69,23 @@ class ProfileController extends Controller
             'cover' => ['nullable', 'image'],
             'avatar' => ['nullable', 'image']
         ]);
+
         $user = $request->user();
+
         $avatar = $data['avatar'] ?? null;
         /** @var \Illuminate\Http\UploadedFile $cover */
         $cover = $data['cover'] ?? null;
+
         if ($cover) {
-            $path = $cover->store('avatars/'.$user->id, 'public');
+            if ($user->cover_path) {
+                Storage::disk('public')->delete($user->cover_path);
+            }
+            $path = $cover->store('user-'.$user->id, 'public');
             $user->update(['cover_path' => $path]);
         }
+
         session('success', 'Cover image has been updated');
+
         return back()->with('status', 'cover-image-update');
     }
 }
