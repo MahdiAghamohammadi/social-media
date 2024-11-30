653302acc3b2208679fdce71de212552e20407c8
MahdiAghamohammadi <mahdi.develoop@gmail.com>
2024-11-30 18:48:14 +0330

    feat: Output posts, followers and followings on user profile page
    Use ckeditor in group about

--- resources/js/Components/app/GroupForm.vue
+++ resources/js/Components/app/GroupForm.vue
@@ -1,11 +1,21 @@
 <script setup>
+
 import InputTextarea from "@/Components/InputTextarea.vue";
 import Checkbox from "@/Components/Checkbox.vue";
 import TextInput from "@/Components/TextInput.vue";
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+
 defineProps({
     form: Object
 })
+
+const editor = ClassicEditor;
+const editorConfig = {
+    toolbar: ['bold', 'italic', '|', 'bulletedList', 'numberedList', '|', 'heading', '|', 'outdent', 'indent', '|', 'link', '|', 'blockQuote'],
+}
+
 </script>
+
 <template>
     <div class="mb-3">
         <label>Group Name</label>
@@ -17,16 +27,20 @@ defineProps({
             autofocus
         />
     </div>
+
     <div class="mb-3">
         <label>
             <Checkbox name="remember" v-model:checked="form.auto_approval"/>
             Enable Auto Approval
         </label>
     </div>
+
     <div class="mb-3">
         <label>About Group</label>
-        <InputTextarea v-model="form.about" class="w-full"/>
+        <ckeditor :editor="editor" v-model="form.about" :config="editorConfig"></ckeditor>
     </div>
 </template>
+
 <style scoped>
+
 </style>
