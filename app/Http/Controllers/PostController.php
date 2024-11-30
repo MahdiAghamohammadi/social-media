85370f09308177449645b6189844693ddf8cab48
MahdiAghamohammadi <mahdi.develoop@gmail.com>
2024-11-30 15:01:07 +0330

    feat: Implement sending notifications on these cases
    When post is created
    When comment is placed
    When post is liked
    When comment is liked

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -11,14 +11,23 @@ use App\Models\Comment;
 use App\Models\Post;
 use App\Models\PostAttachment;
 use App\Models\Reaction;
+use App\Models\User;
+use App\Notifications\CommentCreated;
+use App\Notifications\CommentDeleted;
+use App\Notifications\PostCreated;
+use App\Notifications\PostDeleted;
+use App\Notifications\ReactionAddedOnComment;
+use App\Notifications\ReactionAddedOnPost;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Notification;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Validation\Rule;
 
 class PostController extends Controller
 {
+
     /**
      * Store a newly created resource in storage.
      */
@@ -28,6 +37,7 @@ class PostController extends Controller
         $user = $request->user();
 
         DB::beginTransaction();
+        $allFilePaths = [];
         try {
             $post = Post::create($data);
 
@@ -47,6 +57,14 @@ class PostController extends Controller
             }
 
             DB::commit();
+
+            $group = $post->group;
+
+            if ($group) {
+                $users = $group->approvedUsers()->where('users.id', '!=', $user->id)->get();
+                Notification::send($users, new PostCreated($post, $group));
+            }
+
         } catch (\Exception $e) {
             foreach ($allFilePaths as $path) {
                 Storage::disk('public')->delete($path);
@@ -63,22 +81,25 @@ class PostController extends Controller
      */
     public function update(UpdatePostRequest $request, Post $post)
     {
-        $post->update($request->validated());
-
         $user = $request->user();
+
         DB::beginTransaction();
         $allFilePaths = [];
         try {
             $data = $request->validated();
             $post->update($data);
+
             $deleted_ids = $data['deleted_file_ids'] ?? []; // 1, 2, 3, 4
+
             $attachments = PostAttachment::query()
                 ->where('post_id', $post->id)
                 ->whereIn('id', $deleted_ids)
                 ->get();
+
             foreach ($attachments as $attachment) {
                 $attachment->delete();
             }
+
             /** @var \Illuminate\Http\UploadedFile[] $files */
             $files = $data['attachments'] ?? [];
             foreach ($files as $file) {
@@ -93,6 +114,7 @@ class PostController extends Controller
                     'created_by' => $user->id
                 ]);
             }
+
             DB::commit();
         } catch (\Exception $e) {
             foreach ($allFilePaths as $path) {
@@ -125,10 +147,10 @@ class PostController extends Controller
         return response("You don't have permission to delete this post", 403);
     }
 
-
     public function downloadAttachment(PostAttachment $attachment)
     {
         // TODO check if user has permission to download that attachment
+
         return response()->download(Storage::disk('public')->path($attachment->path), $attachment->name);
     }
 
@@ -155,6 +177,11 @@ class PostController extends Controller
                 'user_id' => $userId,
                 'type' => $data['reaction']
             ]);
+
+            if (!$post->isOwner($userId)) {
+                $user = User::where('id', $userId)->first();
+                $post->user->notify(new ReactionAddedOnPost($post, $user));
+            }
         }
 
         $reactions = Reaction::where('object_id', $post->id)->where('object_type', Post::class)->count();
@@ -171,12 +198,17 @@ class PostController extends Controller
             'comment' => ['required'],
             'parent_id' => ['nullable', 'exists:comments,id']
         ]);
+
         $comment = Comment::create([
             'post_id' => $post->id,
             'comment' => nl2br($data['comment']),
             'user_id' => Auth::id(),
             'parent_id' => $data['parent_id'] ?: null
         ]);
+
+        $post = $comment->post;
+        $post->user->notify(new CommentCreated($comment));
+
         return response(new CommentResource($comment), 201);
     }
 
@@ -196,14 +228,17 @@ class PostController extends Controller
 
         return response("You don't have permission to delete this comment.", 403);
 
+
     }
 
     public function updateComment(UpdateCommentRequest $request, Comment $comment)
     {
         $data = $request->validated();
+
         $comment->update([
             'comment' => nl2br($data['comment'])
         ]);
+
         return new CommentResource($comment);
     }
 
@@ -230,6 +265,11 @@ class PostController extends Controller
                 'user_id' => $userId,
                 'type' => $data['reaction']
             ]);
+
+            if (!$comment->isOwner($userId)) {
+                $user = User::where('id', $userId)->first();
+                $comment->user->notify(new ReactionAddedOnComment($comment->post, $comment, $user));
+            }
         }
 
         $reactions = Reaction::where('object_id', $comment->id)->where('object_type', Comment::class)->count();
